3e4020f283eb90c1c78a9a690d17c114c8bc1a74
Premnaath <premnaathvaradharajan@gmail.com>
2023-10-15 20:46:48 +0530

    Add template structure

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+import streamlit as st
 import tiktoken
 
-encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+# Set the title of the app
+st.title('star-tokenizer')
 
-text = "This is an example sentence to count tokens."
-token_count = len(encoding.encode(text))
-print(f"The text contains {token_count} tokens.")
+# Create a dropdown to select options
+option = st.selectbox('Model', ['Llama 2', 'GPT-3.5'])
+
+# Create a text input for the user to type something
+user_input = st.text_input('Prompt')
+
+# Initialize the token count output
+token_count = 0
+
+# Calculate token count based on the selected option and user input
+if option == 'Llama 2':
+    token_count = len(user_input.split())
+elif option == 'GPT-3.5':
+    token_count = len(user_input.split())
+
+# Display the token count in the output box
+st.markdown(f'**Token Count:** {token_count}')
