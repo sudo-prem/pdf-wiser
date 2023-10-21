8dba1dfdd0183617aa021ca85c949b678ab8d011
Premnaath <premnaathvaradharajan@gmail.com>
2023-10-21 17:13:42 +0530

    Add bert model

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import streamlit as st
 import tiktoken
+from transformers import AutoTokenizer
 
 # Set the title of the app
 st.title('star-tokenizer')
 
 # Create a dropdown to select options
-option = st.selectbox('Model', ['Llama 2', 'GPT-3.5'])
+option = st.selectbox('Model', ['bert-base-uncased'])
 
 # Create a text input for the user to type something
-user_input = st.text_input('Prompt')
+user_input = st.text_area('Prompt', height=7)  # Set height to 7 lines
 
 # Initialize the token count output
 token_count = 0
-
-# Calculate token count based on the selected option and user input
-if option == 'Llama 2':
-    token_count = len(user_input.split())
-elif option == 'GPT-3.5':
-    token_count = len(user_input.split())
+tokenizer = AutoTokenizer.from_pretrained(option)
+tokens = tokenizer.tokenize(tokenizer.decode(tokenizer.encode(user_input)))
+st.markdown(f'{tokens}')
+token_count = len(tokens)
 
 # Display the token count in the output box
 st.markdown(f'**Token Count:** {token_count}')
