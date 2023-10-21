5d014fbd3079a2f9bce4f414a1aa84f94c001545
Dwaraknaath <dwarakvaradharajan@gmail.com>
2023-10-21 23:50:07 +0000

    ST-1: added model input field

--- app.py
+++ app.py
@@ -6,17 +6,23 @@ from transformers import AutoTokenizer
 st.title('star-tokenizer')
 
 # Create a dropdown to select options
-option = st.selectbox('Model', ['bert-base-uncased'])
+option = st.selectbox('Model', ['None', 'bert-base-uncased', 'NousResearch/Llama-2-7b-chat-hf', 'mistralai/Mistral-7B-Instruct-v0.1', 'mistralai/Mistral-7B-v0.1'])
+if option == 'None':
+    model_name = st.text_input("Enter hugging face model name")
+else:
+    model_name = option
 
-# Create a text input for the user to type something
-user_input = st.text_area('Prompt', height=7)  # Set height to 7 lines
+if model_name:
 
-# Initialize the token count output
-token_count = 0
-tokenizer = AutoTokenizer.from_pretrained(option)
-tokens = tokenizer.tokenize(tokenizer.decode(tokenizer.encode(user_input)))
-st.markdown(f'{tokens}')
-token_count = len(tokens)
+    # Create a text input for the user to type something
+    user_input = st.text_area('Prompt', height=512)  # Set height to 7 lines
 
-# Display the token count in the output box
-st.markdown(f'**Token Count:** {token_count}')
+    # Initialize the token count output
+    token_count = 0
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokens = tokenizer.tokenize(tokenizer.decode(tokenizer.encode(user_input)))
+    st.markdown(f'{tokens}')
+    token_count = len(tokens)
+
+    # Display the token count in the output box
+    st.markdown(f'**Token Count:** {token_count}')
