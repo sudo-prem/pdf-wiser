6becd7275b56cebaaa5c11392aa9a0f97a7ae622
Premnaath <premnaathvaradharajan@gmail.com>
2023-10-22 11:08:46 +0530

    Generic Refactor

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import streamlit as st
-import tiktoken
 from transformers import AutoTokenizer
 
 # Set the title of the app
 st.title('star-tokenizer')
 
 # Create a dropdown to select options
-option = st.selectbox('Model', ['None', 'bert-base-uncased', 'NousResearch/Llama-2-7b-chat-hf', 'mistralai/Mistral-7B-Instruct-v0.1', 'mistralai/Mistral-7B-v0.1'])
+option = st.selectbox('Model', ['None', 'bert-base-uncased', 'NousResearch/Llama-2-7b-chat-hf',
+                      'mistralai/Mistral-7B-Instruct-v0.1', 'mistralai/Mistral-7B-v0.1'])
 if option == 'None':
     model_name = st.text_input("Enter hugging face model name")
 else:
     model_name = option
 
 if model_name:
-
     # Create a text input for the user to type something
     user_input = st.text_area('Prompt', height=512)  # Set height to 7 lines
 
