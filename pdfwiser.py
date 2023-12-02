2447e9522f30d3ced99dc59fc5b795d0a549504d
Premnaath <premnaathvaradharajan@gmail.com>
2023-12-02 16:58:39 +0530

    tokenizer and pdfwiser to pages

---
+++ pdfwiser.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def pdfwiser():
+    # Your PDF Util page content goes here
+    st.title('PDF Util Page')
+    st.write(
+        "This is the PDF Util page. You can add your PDF-related functionality here.")
