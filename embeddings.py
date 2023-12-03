803c625e1f68c54528992514a6dd58c874ba7213
Dwaraknaath <dwarakvaradharajan@gmail.com>
2023-12-03 17:24:52 +0000

    ST-10: added test case for embeddings

--- embeddings.py
+++ embeddings.py
@@ -18,29 +18,34 @@ def recursive_splitter(data):
         return texts
 
 class EmbeddingSearchEngine:
-    def __init__(self, model_name='paraphrase-distilroberta-base-v1', model_type="SentenceTransformer"):
+    def __init__(self, storage_location="./chroma_db", model_name='paraphrase-distilroberta-base-v1', model_type="SentenceTransformer"):
         if model_type == "SentenceTransformer":
             self.model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
         else:
             self.model = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
         self.vector_db = None
-        self.storage_location = None
+        self.storage_location = storage_location
 
     def create_embeddings_for_pdf(self, pdf_file):
         loader = PyMuPDFLoader(pdf_file)
         data = loader.load()
         texts = recursive_splitter(data)
-        self.vector_db = Chroma.from_documents(texts, self.model)
+        self.vector_db = Chroma.from_documents(texts, self.model, persist_directory=self.storage_location)
         return self
 
     def search_embeddings(self, query):
+
+        if self.vector_db == None:
+             print("Vector db does not exists")
+             return ""
         docs = self.vector_db.similarity_search(query)
         return docs
 
 
 if __name__ == "__main__":
-    embedding_engine = EmbeddingSearchEngine()
-    pdf_file = "sodapdf-converted.pdf"
-    embedding_engine = embedding_engine.create_embeddings_for_pdf(pdf_file=pdf_file)
+    pdf_file_path = "sodapdf-converted.pdf"
+
+    embedding_engine = EmbeddingSearchEngine(storage_location=pdf_file_path.split(".pdf")[0])
+    embedding_engine = embedding_engine.create_embeddings_for_pdf(pdf_file=pdf_file_path)
     print(embedding_engine.search_embeddings("What did Clara stumble upon?"))
 
