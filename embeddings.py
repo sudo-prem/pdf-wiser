954ea02983455bc65937142a959a6c25607d46af
Premnaath <premnaathvaradharajan@gmail.com>
2024-02-18 13:40:00 +0530

    Update gitignore, display result on screen

--- embeddings.py
+++ embeddings.py
@@ -1,29 +1,28 @@
-from sentence_transformers import SentenceTransformer
+import streamlit as st
+import os
+
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceInstructEmbeddings
-import os
 
 def recursive_document_splitter(data):
     text_splitter = RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
-        chunk_size = 200,
-        chunk_overlap  = 20,
-        length_function = len,
-        is_separator_regex = False,
+        chunk_size=200,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
     )
     texts = text_splitter.split_documents(data)
     return texts
 
 def recursive_text_splitter(data):
     text_splitter = RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
-        chunk_size = 200,
-        chunk_overlap  = 20,
-        length_function = len,
-        is_separator_regex = False,
+        chunk_size=200,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
     )
     texts = text_splitter.split_text(data)
     return texts
@@ -41,9 +40,7 @@ class EmbeddingSearchEngine:
         self.storage_location = storage_location
 
         if vector_db_exists(storage_location):
-            self.vector_db  = Chroma(persist_directory=storage_location, embedding_function=self.model)
-
-
+            self.vector_db = Chroma(persist_directory=storage_location, embedding_function=self.model)
 
     def create_embeddings_for_pdf(self, pdf_file_path):
         loader = PyMuPDFLoader(pdf_file_path)
@@ -58,23 +55,37 @@ class EmbeddingSearchEngine:
         return self
 
     def search_embeddings(self, query):
-
-        if self.vector_db == None:
-             print("Vector db does not exists")
-             return ""
+        if self.vector_db is None:
+            print("Vector db does not exist")
+            return ""
         docs = self.vector_db.similarity_search(query)
         return docs
     
     def check_if_embeddings_exist(self):
         return vector_db_exists(self.storage_location)
 
-
 if __name__ == "__main__":
-    pdf_file_path = "sodapdf-converted.pdf"
+    st.title("PDF Wiser")
+    
+    # File picker
+    uploaded_file = st.file_uploader("Upload PDF file", type="pdf")
 
-    embedding_engine = EmbeddingSearchEngine(storage_location=pdf_file_path.split(".pdf")[0])
-    if not embedding_engine.check_if_embeddings_exist():
-        print("Embeddings does not exist, so creating one")
-        embedding_engine = embedding_engine.create_embeddings_for_pdf(pdf_file_path=pdf_file_path)
-    print(embedding_engine.search_embeddings("What did Clara stumble upon?"))
+    # Text input for query
+    query = st.text_input("Enter your query")
 
+    if uploaded_file is not None:
+        pdf_file_path = uploaded_file.name
+
+        embedding_engine = EmbeddingSearchEngine(storage_location=pdf_file_path.split(".pdf")[0])
+        if not embedding_engine.check_if_embeddings_exist():
+            st.write("Embeddings do not exist, so creating one...")
+            embedding_engine = embedding_engine.create_embeddings_for_pdf(pdf_file_path=pdf_file_path)
+        
+        if st.button("Submit"):
+            search_results = embedding_engine.search_embeddings(query)
+            if search_results:
+                first_result_content = search_results[0].page_content
+                st.write("Most relevant answer:")
+                st.write(first_result_content)
+            else:
+                st.write("No relevant answers found.")
